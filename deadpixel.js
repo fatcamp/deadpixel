fc106634146e94ca0f497d2adfc978c990140a77
Dan Hoffman <dhoffman@brownbagmarketing.com>
2013-02-10 12:32:35 -0500

    Added window 'onload' event listener

--- deadpixel.js
+++ deadpixel.js
@@ -1,41 +1,50 @@
 /*
     NAME:     DEADPIXEL.JS
     FUNCTION: PUTS AN ANNOYING RED PIXEL ON THE SCREEN
-    USAGE:    INCLUDE SCRIPT JUST BEFORE THE CLOSING </BODY> TAG 
-    VERSION:  1.0
+    VERSION:  1.1
     AUTHOR:   DAN HOFFMAN
     EMAIL:    DHOFFMAN@OUTLOOK.COM
 */
 
-(function(d, undefined) {
-    // Pixel x and y.
-    var pixel_x, pixel_y;
+(function(w, d, undefined) {
 
-    // Parse viewport width and height.
-    var width = d.documentElement ? d.documentElement.clientWidth : 0;
-    var height = d.documentElement ? d.documentElement.clientHeight : 0;
+    function AddPixel() {
+        // Pixel x and y.
+        var pixel_x, pixel_y;
 
-    // If the browser doesn't suck...
-    if(width != 0 && height != 0) {
-        // Grab a random x and y value from width and height
-        pixel_x = Math.floor((Math.random()*width)+1);
-        pixel_y = Math.floor((Math.random()*height)+1);
+        // Parse viewport width and height.
+        var width = d.documentElement ? d.documentElement.clientWidth : 0;
+        var height = d.documentElement ? d.documentElement.clientHeight : 0;
 
-        // Create our dead pixel.
-        var pixel = d.createElement("div");
-            pixel.style.width = "1px";
-            pixel.style.height = "1px";
-            pixel.style.background = "red";
-            pixel.style.position = "fixed";
-            pixel.style.top = pixel_y + "px";
-            pixel.style.right = pixel_x + "px";
+        // If the browser doesn't suck...
+        if(width != 0 && height != 0) {
+            // Grab a random x and y value from width and height
+            pixel_x = Math.floor((Math.random()*width)+1);
+            pixel_y = Math.floor((Math.random()*height)+1);
 
-            // 16-bit signed integer for old phones
-            pixel.style.zIndex = "65536"; 
+            // Create our dead pixel.
+            var pixel = d.createElement("div");
+                pixel.style.width = "1px";
+                pixel.style.height = "1px";
+                pixel.style.background = "red";
+                pixel.style.position = "fixed";
+                pixel.style.top = pixel_y + "px";
+                pixel.style.right = pixel_x + "px";
 
-        // Add dead pixel to our page body.
-        d.body.appendChild(pixel);
+                // 16-bit signed integer for old phones
+                pixel.style.zIndex = "65536"; 
+
+            // Add dead pixel to our page body.
+            d.body.appendChild(pixel);
+        }
+    }
+    
+    // Add event listener to the window.
+    if(!w.addEventListener) {
+        w.attachEvent("onload",AddPixel);
+    } else {
+        w.addEventListener("load",AddPixel,false);
     }
 
-// Pass in document and undefined   
-})(this.document);
+// Pass in document and undefined.   
+})(this, this.document);
