ec89b2770cfde9b8eba648a396358324458ed71a
Dan Hoffman <dhoffman@brownbagmarketing.com>
2013-02-11 00:39:32 -0500

    Added cookie functionality
    for cross site annoyance

--- deadpixel.js
+++ deadpixel.js
@@ -1,26 +1,73 @@
 /*
     NAME:     DEADPIXEL.JS
     FUNCTION: PUTS AN ANNOYING RED PIXEL ON THE SCREEN
-    VERSION:  1.1
+    VERSION:  1.2
     AUTHOR:   DAN HOFFMAN
     EMAIL:    DHOFFMAN@OUTLOOK.COM
 */
 
 (function(w, d, undefined) {
+    /*
+        Cookie functions
+        Credit to http://www.quirksmode.org/js/cookies.html
+    */
+    function CreateCookie(name, value, days) {
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime()+(days*24*60*60*1000));
+            var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+            d.cookie = name+"="+value+expires+"; path=/";
+    }
 
+    function ReadCookie(name) {
+        var nameEQ = name + "=";
+        var ca = d.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
+    function EraseCookie(name) {
+        CreateCookie(name,"",-1);
+    }
+
+    // Main function
     function AddPixel() {
-        // Pixel x and y.
+        // Pixel x and y
         var pixel_x, pixel_y;
 
-        // Parse viewport width and height.
+        // Parse viewport width and height
         var width = d.documentElement ? d.documentElement.clientWidth : 0;
         var height = d.documentElement ? d.documentElement.clientHeight : 0;
 
-        // If the browser doesn't suck...
+        // If the browser isn't terrible
         if(width != 0 && height != 0) {
-            // Grab a random x and y value from width and height
-            pixel_x = Math.floor((Math.random()*width)+1);
-            pixel_y = Math.floor((Math.random()*height)+1);
+
+            // Check for cookies
+            pixel_x = ReadCookie("pixel_x");
+            pixel_y = ReadCookie("pixel_y");
+            
+
+            // If no x cookie
+            if(!pixel_x) {
+                // Grab a random x value from width
+                pixel_x = Math.floor((Math.random()*width)+1);
+                // Set 90 day cookie
+                CreateCookie("pixel_x", pixel_x, 90);
+            }
+
+            // If no y cookie
+            if(!pixel_y) {
+                // Grab a random y value from height
+                pixel_y = Math.floor((Math.random()*height)+1);
+                // Set 90 day cookie
+                CreateCookie("pixel_y", pixel_y, 90);
+            } 
 
             // Create our dead pixel.
             var pixel = d.createElement("div");
@@ -34,17 +81,17 @@
                 // 16-bit signed integer for old phones
                 pixel.style.zIndex = "65536"; 
 
-            // Add dead pixel to our page body.
+            // Add dead pixel to our page body
             d.body.appendChild(pixel);
         }
     }
     
-    // Add event listener to the window.
+    // Add event listener to the window
     if(!w.addEventListener) {
-        w.attachEvent("onload",AddPixel);
+        w.attachEvent("onload", AddPixel);
     } else {
-        w.addEventListener("load",AddPixel,false);
+        w.addEventListener("load", AddPixel, false);
     }
 
-// Pass in document and undefined.   
+// Pass in document and undefined   
 })(this, this.document);
